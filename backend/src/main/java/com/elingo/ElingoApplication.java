d0795606510e6bdd7e54a989d518ea17f39fbfcc
DuyVo2005 <vlkd1001@gmail.com>
2026-08-27 12:49:29 +0700

    basic structure

---
+++ backend/src/main/java/com/elingo/ElingoApplication.java
@@ -0,0 +1,13 @@
+package com.elingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElingoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElingoApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/elingo/ElingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElingoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
